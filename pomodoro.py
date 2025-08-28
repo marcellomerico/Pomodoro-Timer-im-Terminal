3bf16371b06b1fba4c89e091bd87263c0fd48c49
Marcello Antonio Merico <merico80@gmail.com>
2025-08-28 15:05:50 +0200

    User Input hinzugefügt

--- pomodoro.py
+++ pomodoro.py
@@ -10,17 +10,35 @@ def countdown(minutes):
         seconds -= 1  
     print("\nZeit ist um!")
 
+def user_input():
+    while True:
+        try:
+            minutes = int(input("Gib die Anzahl der Minuten ein (1-60): "))
+            if 1 <= minutes <= 60:
+                return minutes
+            else:
+                print("Bitte eine Zahl zwischen 1 und 60 eingeben.")
+        except ValueError:
+            print("Ungültige Eingabe. Bitte eine Zahl eingeben.")
+         
+
+print("Willkommen zum Pomodoro Timer!")
+print("Du wirst 4 Arbeitsphasen und 3 Pausenphasen durchlaufen.")
+print("Bitte gib die Dauer für Arbeits- und Pausenphasen ein.")
+arbeitszeit = user_input()
+print("Jetzt gib die Dauer für die Pausenphasen ein.")
+pausezeit = user_input()
 
 print("Fokus beginnt jetzt, viel Erfolg!")
-countdown(25)
+countdown(arbeitszeit)
 print("Erste kurze Pause, entspann dich!")
-countdown(5)
+countdown(pausezeit)
 print("Zurück zur Arbeit, du schaffst das!")
-countdown(25)
+countdown(arbeitszeit)
 print("Zweite kurze Pause, tief durchatmen!")
-countdown(5)
+countdown(pausezeit)
 print("Letzte Arbeitsphase, gib alles!")
-countdown(25)
+countdown(arbeitszeit)
 print("Große Pause, genieße deine Zeit!")
-countdown(15)
+countdown(pausezeit)
 print("Pomodoro-Zyklus abgeschlossen! Gute Arbeit!")
